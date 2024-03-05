dc859ffc899f1f8ba85ebd1e482dd3ae9109c7ea
Carlos Pinho <carlospinhod@gmail.com>
2024-03-05 23:16:59 +0000

    Database base structure
    Initial Database structure

---
+++ database/migrations/2024_03_05_202058_create_links_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Links', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('profile_id')->constrained('profiles')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('url');
+            $table->integer('position')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
---
+++ database/migrations/2024_03_05_203052_create_social_links_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('social_links', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('social_network_id')->constrained('social_networks')->cascadeOnDelete();
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('social_links');
+    }
+};
