7b10c95f3f4de7a2a5e6c4f2f422e850a68f717f
Carlos Pinho <carlospinhod@gmail.com>
2024-03-08 00:08:33 +0000

    Added icon css classes
    Added icon css classes after decision to use bootstrap as css framework

--- database/seeders/SocialNetworkSeeder.php
+++ database/seeders/SocialNetworkSeeder.php
@@ -14,17 +14,17 @@ class SocialNetworkSeeder extends Seeder
     public function run(): void
     {
         $socialNetworks = [
-            ['name' => 'Facebook', 'base_domain' => 'https://www.facebook.com/', 'icon' => ''],
-            ['name' => 'Instagram', 'base_domain' => 'https://www.instagram.com/', 'icon' => ''],
-            ['name' => 'Twitter', 'base_domain' => 'https://twitter.com/', 'icon' => ''],
-            ['name' => 'LinkedIn', 'base_domain' => 'https://www.linkedin.com/', 'icon' => ''],
-            ['name' => 'Snapchat', 'base_domain' => 'https://www.snapchat.com/', 'icon' => ''],
-            ['name' => 'Pinterest', 'base_domain' => 'https://www.pinterest.com/', 'icon' => ''],
-            ['name' => 'TikTok', 'base_domain' => 'https://www.tiktok.com/', 'icon' => ''],
-            ['name' => 'YouTube', 'base_domain' => 'https://www.youtube.com/', 'icon' => ''],
-            ['name' => 'WhatsApp', 'base_domain' => 'https://www.whatsapp.com/', 'icon' => ''],
-            ['name' => 'Reddit', 'base_domain' => 'https://www.reddit.com/', 'icon' => ''],
-            ['name' => 'Threads', 'base_domain' => 'https://www.threads.net/', 'icon' => ''],
+            ['name' => 'Facebook', 'base_domain' => 'https://www.facebook.com/', 'icon' => 'bi-facebook'],
+            ['name' => 'Instagram', 'base_domain' => 'https://www.instagram.com/', 'icon' => 'bi-instagram'],
+            ['name' => 'Twitter', 'base_domain' => 'https://twitter.com/', 'icon' => 'bi-twitter'],
+            ['name' => 'LinkedIn', 'base_domain' => 'https://www.linkedin.com/', 'icon' => 'bi-linkedin'],
+            ['name' => 'Snapchat', 'base_domain' => 'https://www.snapchat.com/', 'icon' => 'bi-snapchat'],
+            ['name' => 'Pinterest', 'base_domain' => 'https://www.pinterest.com/', 'icon' => 'bi-pinterest'],
+            ['name' => 'TikTok', 'base_domain' => 'https://www.tiktok.com/', 'icon' => 'bi-tiktok'],
+            ['name' => 'YouTube', 'base_domain' => 'https://www.youtube.com/', 'icon' => 'bi-youtube'],
+            ['name' => 'WhatsApp', 'base_domain' => 'https://www.whatsapp.com/', 'icon' => 'bi-whatsapp'],
+            ['name' => 'Reddit', 'base_domain' => 'https://www.reddit.com/', 'icon' => 'bi-reddit'],
+            ['name' => 'Threads', 'base_domain' => 'https://www.threads.net/', 'icon' => 'bi-alarm'],
         ];
 
         foreach ($socialNetworks as $network) {
