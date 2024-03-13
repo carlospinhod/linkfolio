f3c457e74ae66fc9b8695b3eff039096933f279d
Carlos Pinho <carlospinhod@gmail.com>
2024-03-13 00:09:44 +0000

    mistake fix
    small mistake fix

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\ProfileController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '';
 });
 
 Route::get('/{username}', [ProfileController::class, 'index']);
